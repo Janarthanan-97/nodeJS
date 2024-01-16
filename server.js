12e99160c102b2bd46d2ddc5e2225e6a6cf8caa7
Janarthanan-97 <janamadhav13@gmail.com>
2024-01-17 00:58:20 +0530

    backend testing for render

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const cors = require('cors')
 
@@ -11,12 +9,11 @@ const itemRouter = require('./routes/itemRoutes')
 const orderRouter = require('./routes/orderRouter')
 const userRouter = require('./routes/userRouter')
 
-
-
-
-
 app.use('/items', itemRouter);
 app.use('/orders', orderRouter);
 app.use('/users', userRouter);
+app.get('/', async(req, res)=>{
+    res.send("welcome to Inventory app")
+})
 
 app.listen(3001)
